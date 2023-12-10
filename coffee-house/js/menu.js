fa24389f42cd072fcada357b93e30d5a447d1805
DisRachik <radchenkoig@gmail.com>
2023-12-10 12:19:25 +0200

    feat: add filtering by product categories

--- coffee-house/js/menu.js
+++ coffee-house/js/menu.js
@@ -1,16 +1,27 @@
 import { handleMenuToggle } from './mobile-menu.js';
 import { cardGallery } from './gallery/card-gallery.js';
+import { filterCategory, sortByValue } from './gallery/sort-by-value.js';
 
 const gallery = document.querySelector('.gallery');
+const filterList = document.querySelector('.filter');
+let products = [];
 
 handleMenuToggle();
 
 fetch('./data/products.json')
   .then((res) => res.json())
   .then((res) => {
-    const cards = cardGallery(res);
-    gallery.innerHTML = cards;
+    products.push(...res);
+    console.log(products);
+
+    const cards = sortByValue(products, filterCategory(filterList));
+    gallery.innerHTML = cardGallery(cards);
   })
   .catch((error) => {
     console.error('Error fetching data:', error);
   });
+
+filterList.addEventListener('change', (event) => {
+  const cards = sortByValue(products, filterCategory(filterList));
+  gallery.innerHTML = cardGallery(cards);
+});
